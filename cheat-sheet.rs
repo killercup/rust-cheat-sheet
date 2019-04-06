d4705136cd5a38a459017f1168e0c0c411956a31
Pascal Hertleif <pascal@technocreatives.com>
2019-04-06 19:25:54 +0200

    to_owned, TOC

--- cheat-sheet.rs
+++ cheat-sheet.rs
@@ -1,9 +1,9 @@
 /// The `main` function will be executed first in a binary.
 /// Triple-slash comments are documentation -- check out `cargo doc`
 fn main() {
-    getting_started();
-    custom_types();
-    ownership_and_borrowing();
+    getting_started();         // basic syntax, simple types
+    custom_types();            // define custom types and add methods to them.
+    ownership_and_borrowing(); // *the* feature of Rust
 }
 
 fn getting_started() {
@@ -12,7 +12,7 @@ fn getting_started() {
     y += 1;                    // Add 1 to `y`, making it 3
 
     let x = "Hello";           // A reference to a string slice (`&str`)
-    let mut y = x.to_string(); // Copy the text into a mutable `String`.
+    let mut y = x.to_owned(); // Copy the text into a mutable `String`.
     y.push_str(" World!");     // Now, we can change it!
 
     let xs = vec![7, 3, 8];    // A list (vector) of integers: `Vec<i32>`
@@ -21,21 +21,20 @@ fn getting_started() {
     }                          // …the value of `x` (using the `Display` trait)
 }
 
-/// Let's see how to define custom types and add methods to them.
 fn custom_types() {
     struct Person {            // Define a new data type with two fields,…
         name: String,          // `name`, which contains a `String`, and
         age: u16,              // `age`, which is a unsigned 16bit integer
     }
 
-    let jim = Person { name: "Jim".to_string(), age: 25 };
+    let jim = Person { name: "Jim".to_owned(), age: 25 };
 
     impl Person {              // Add inherent methods to the type
         fn new(name: &str, age: u16) -> Person {
             let name = if name.is_empty() { // `if` is an expression.
-                "anonymous".to_string()     // Both branches need to return
+                "anonymous".to_owned()     // Both branches need to return
             } else {                        // the same type
-                name.to_string()
+                name.to_owned()
             };                              // Oh, and names can be shadowed
 
             Person { name, age } // Shortcut syntax!
@@ -53,16 +52,15 @@ fn custom_types() {
     println!("{}", jim);
 }
 
-/// *The* feature of Rust
 fn ownership_and_borrowing() {
     fn this_borrows(x: &str) -> bool {
         x.len() > 5
     }
-    let name = "Gustav".to_string();
+    let name = "Gustav".to_owned();
     this_borrows(&name);            // Pass a reference to the function
     println!("{}", name);           // `name` is still available, and unchanged
 
-    let mut name = "Johan".to_string();
+    let mut name = "Johan".to_owned();
     fn this_borrows_mutable(x: &mut String) {
         x.push_str(", nice to meet you!");
     }
@@ -70,9 +68,9 @@ fn ownership_and_borrowing() {
     println!("{}", name);           // "Johan, nice to meet you!"
 
     fn this_consumes(x: String) -> String {
-        "I ate your string!".to_string()
+        "I ate your string!".to_owned()
     }
-    let name = "Peter".to_string();
+    let name = "Peter".to_owned();
     let data = this_consumes(name); // After this, `name` will be unavailable!
     //> this_borrows(&name);        // Error: "borrow of moved value: `name`"
     println!("{}", data);           // The compiler errors are nice. Try it!
